05bbf161400e0c86f05548d29d5a15fd6c5db367
iamanoopsingh <87091574+iamanoopsingh@users.noreply.github.com>
2026-01-29 22:57:55 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const matches = [
+  {
+    id: 1,
+    team1: "India",
+    team2: "Australia",
+    date: "20 July 2026",
+    venue: "Mumbai",
+    status: "Upcoming"
+  },
+  {
+    id: 2,
+    team1: "England",
+    team2: "Pakistan",
+    date: "25 July 2026",
+    venue: "London",
+    status: "Upcoming"
+  }
+];
+
+const tournaments = [
+  {
+    id: 1,
+    name: "Summer Cup",
+    location: "India",
+    teams: ["India", "Australia", "England"]
+  }
+];
